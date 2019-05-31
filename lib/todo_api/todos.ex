e1738462df28da71310698e116be7ea3e9473841
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-05-31 02:45:24 -0300

    Add TodoApi.CurrentUser context

--- lib/todo_api/todos.ex
+++ lib/todo_api/todos.ex
@@ -10,15 +10,8 @@ defmodule TodoApi.Todos do
   alias TodoApi.Accounts.User
 
   def list_todos, do: Repo.all(Todo)
-  def list_todos(%User{id: id}) do
-    from(t in Todo, where: t.owner_id == ^id)
-    |> Repo.all()
-  end
 
   def get_todo!(id), do: Repo.get!(Todo, id)
-  def get_todo!(id, %User{id: user_id}) do
-    Repo.get_by!(Todo, [id: id, owner_id: user_id])
-  end
 
   def create_todo(attrs \\ %{}) do
     %Todo{}
